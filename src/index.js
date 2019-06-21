2994f1a395d1e9b829783065584727ab89d8f3f8
Mikhail Petrov <mihail.petrov@introlab-systems.com>
2019-06-21 15:47:28 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/common'
+import './css/main.css'
+
+// import Vue from 'vue'
+window.Vue = require('vue')
+
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+          test: /\.scss$/,
+          use: [
+              'style-loader',
+              MiniCssExtractPlugin.loader,
+              {
+                  loader: 'css-loader',
+                  options: {
+                      sourceMap: true,
+                  }
+              }
+          ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    overlay: true, // * Вывод ошибок в браузере
+  },
+  plugins: [
+      new MiniCssExtractPlugin({
+          filename: '[name].css',
+        //   chunkFilename: '[id].css'
+      })
+  ]
+}
